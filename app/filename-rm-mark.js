6f784702e831400d715391b80129aba1b6c0691f
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-15 00:20:46 +0800

    Wed Feb 15 12:20:46 AM CST 2023

---
+++ app/filename-rm-mark.js
@@ -0,0 +1,27 @@
+const ShellSpawn = require('./lib/ShellSpawn')
+const ShellExec = require('./lib/ShellExec')
+const GetExistedArgv = require('./lib/GetExistedArgv')
+
+const path = require('path')
+const fs = require('fs')
+
+let main = async function () {
+  let files = GetExistedArgv()
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+    
+    let filename = path.basename(file)
+    let dirname = path.dirname(file)
+    let filenameNoExt = path.parse(filename).name
+    let ext = path.extname(filename)
+    if (filenameNoExt.indexOf('-') === -1) {
+      continue
+    }
+
+    let newFilenameNoExt = filenameNoExt.slice(0, filenameNoExt.lastIndexOf('-'))
+
+    await ShellExec(`mv "${file}" "${dirname}/${newFilenameNoExt}${ext}"`)
+  }
+}
+
+main()
---
+++ index.js
@@ -0,0 +1,70 @@
+const ShellSpawn = require('./app/lib/ShellSpawn')
+const GetExistedArgv = require('./app/lib/GetExistedArgv')
+const SetDockerComposeYML = require('./app/lib/SetDockerComposeYML')
+
+// const dialog = require('node-file-dialog')
+
+const fs = require('fs')
+const path = require('path')
+
+const lockFile = path.resolve(__dirname, '.lock')
+let lock = function () {
+	fs.writeFileSync(lockFile, (new Date().toString()))
+}
+
+let isLocked = function () {
+	return fs.existsSync(lockFile)
+}
+
+let unlock = function () {
+	fs.unlinkSync(lockFile)
+}
+
+function sleep(ms = 500) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let main = async function () {
+	// while (isLocked()) {
+	// 	await sleep()
+	// }
+	// lock()
+
+  // 1. 先取得輸入檔案的列表
+  let files = GetExistedArgv()
+
+  // console.log('gogogo', files)
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+
+		// ------------
+
+		// let filename = path.basename(file)
+		// let dirname = path.dirname(file)
+		// let tempFilename = Math.floor(Math.random() * 100000) + '.zip'
+		
+		// let tempFilePath = path.resolve(dirname, tempFilename)
+		// // console.log(tempFilePath)
+		// // console.log(path.resolve(dirname, filename.slice(0, -4) + '.txt'))
+
+		// fs.renameSync(file, tempFilePath)
+
+		// ------------
+    
+    // SetDockerComposeYML(tempFilePath)
+		SetDockerComposeYML(file)
+    await ShellSpawn('docker-compose up')
+
+		// --------------
+
+		// fs.renameSync(tempFilePath, file)
+		// if (fs.existsSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf')) {
+		// 	fs.renameSync(path.resolve(dirname, tempFilename.slice(0, -4)) + '.pdf', path.resolve(dirname, filename.slice(0, -4)) + '.pdf')
+		// }
+  }
+
+	// unlock()
+
+}
+
+main()
